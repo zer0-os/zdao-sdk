b050424476ed2012bee7f18b9454b4c0cf0a7bcf
deep-quality-dev <hungrywarrior081@gmail.com>
2022-03-24 00:21:51 +1000

    fix: throw NotImplementedError on the contract related functions

--- src/zNA/index.ts
+++ src/zNA/index.ts
@@ -2,8 +2,10 @@ import { ethers } from 'ethers';
 
 import zDAOCore from '../config/constants/abi/zDAOCore.json';
 import { zDAOId, zNA, zNAConfig } from '../types';
+import { t } from '../utilities/messages';
 
 export const createClient = (config: zNAConfig) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const contract = new ethers.Contract(
     config.contract,
     zDAOCore,
@@ -14,24 +16,28 @@ export const createClient = (config: zNAConfig) => {
    * Get all the list of zDAO
    */
   const listZNA = async (): Promise<zNA[]> => {
-    return await contract.getDAOIds();
+    // return await contract.getDAOIds();
+    throw Error(t('not-implemented'));
   };
 
   /**
    * Get zDAO by zNA
    * @param zNA zNA address to find zDAO
    */
-  const getZDAOIdByZNA = async (zNA: zNA): Promise<zDAOId> => {
-    return await contract.zNATozDAO(zNA);
+  const getZDAOIdByZNA = async (_: zNA): Promise<zDAOId> => {
+    // return await contract.zNATozDAO(zNA);
+    throw Error(t('not-implemented'));
   };
 
-  const getDAOMetadataUri = async (zDAOId: zDAOId): Promise<string> => {
-    return await contract.getDAOMetadataUri(zDAOId);
+  const getDAOMetadataUri = async (_: zDAOId): Promise<string> => {
+    // return await contract.getDAOMetadataUri(zDAOId);
+    throw Error(t('not-implemented'));
   };
 
-  const doesZDAOExist = async (zNA: zNA): Promise<boolean> => {
-    const daoId = await contract.zNATozDAO(zNA);
-    return daoId.length > 0 ? true : false;
+  const doesZDAOExist = async (_: zNA): Promise<boolean> => {
+    // const daoId = await contract.zNATozDAO(zNA);
+    // return daoId.length > 0 ? true : false;
+    throw Error(t('not-implemented'));
   };
 
   return {
