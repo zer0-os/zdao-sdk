e5c9ba84796e211eceab881f61d97fbbc526afa7
deep-quality-dev <hungrywarrior081@gmail.com>
2022-09-02 09:07:49 -0400

    refactor: remove fetching zDAO contract address on Ethereum

--- src/polygon/client/DAOClient.ts
+++ src/polygon/client/DAOClient.ts
@@ -59,29 +59,10 @@ class DAOClient
     super(properties, gnosisSafeClient);
     this.zDAOOptions = cloneDeep(properties);
 
-    return (async (): Promise<DAOClient> => {
-      this.rootTokenContract = IERC20Upgradeable__factory.connect(
-        properties.votingToken.token,
-        GlobalClient.etherRpcProvider
-      );
-
-      const promises: Promise<any>[] = [
-        GlobalClient.ethereumZDAOChef.getZDAOById(this.properties.id),
-        this.getPolygonZDAOContract(),
-      ];
-      const results = await Promise.all(promises);
-
-      this.ethereumZDAO = results[0] as EthereumZDAO;
-      this.polygonZDAO = results[1] as PolygonZDAOContract;
-
-      if (this.polygonZDAO) {
-        this.properties.state = zDAOState.ACTIVE;
-      }
-      if (properties.destroyed) {
-        this.properties.state = zDAOState.CANCELED;
-      }
-      return this;
-    })() as unknown as DAOClient;
+    this.rootTokenContract = IERC20Upgradeable__factory.connect(
+      properties.votingToken.token,
+      GlobalClient.etherRpcProvider
+    );
   }
 
   get polygonToken() {
@@ -96,15 +77,19 @@ class DAOClient
       GlobalClient.ethereumZDAOChef.getZDAOInfoById(zDAORecord.id),
       GlobalClient.polygonZDAOChef.getZDAOInfoById(zDAORecord.id),
     ]);
-    if (!zDAOInfos[0]) {
+
+    const etherZDAOInfo = zDAOInfos[0];
+    const polyZDAOInfo = zDAOInfos[1];
+
+    if (!etherZDAOInfo) {
       throw new NotFoundError(errorMessageForError('not-found-zdao'));
     }
 
     const tokens = await Promise.all([
-      getToken(GlobalClient.etherRpcProvider, zDAOInfos[0].token),
-      zDAOInfos[1] &&
-        getToken(GlobalClient.polyRpcProvider, zDAOInfos[1].token),
-      getTotalSupply(GlobalClient.etherRpcProvider, zDAOInfos[0].token).catch(
+      getToken(GlobalClient.etherRpcProvider, etherZDAOInfo.token),
+      polyZDAOInfo &&
+        getToken(GlobalClient.polyRpcProvider, polyZDAOInfo.token),
+      getTotalSupply(GlobalClient.etherRpcProvider, etherZDAOInfo.token).catch(
         () => {
           throw new InvalidError(
             errorMessageForError('not-support-total-supply')
@@ -113,8 +98,7 @@ class DAOClient
       ),
     ]);
 
-    const etherZDAOInfo = zDAOInfos[0];
-    const instance = await new DAOClient(
+    const instance = new DAOClient(
       {
         id: zDAORecord.id,
         zNAs: zDAORecord.associatedzNAs,
@@ -132,7 +116,11 @@ class DAOClient
         minimumTotalVotingTokens:
           etherZDAOInfo.minimumTotalVotingTokens.toString(),
         isRelativeMajority: etherZDAOInfo.isRelativeMajority,
-        state: zDAOState.PENDING,
+        state: etherZDAOInfo.destroyed
+          ? zDAOState.CANCELED
+          : polyZDAOInfo
+          ? zDAOState.ACTIVE
+          : zDAOState.PENDING,
         snapshot: etherZDAOInfo.snapshot,
         destroyed: etherZDAOInfo.destroyed,
         polygonToken: tokens[1] as Token,
@@ -311,16 +299,13 @@ class DAOClient
     try {
       const ipfs = await AbstractDAOClient.uploadToIPFS(signer, payload);
 
-      await GlobalClient.ethereumZDAOChef.createProposal(
+      const proposalId = await GlobalClient.ethereumZDAOChef.createProposal(
         signer,
         this.id,
         payload,
         ipfs
       );
-
-      // created proposal id
-      const lastProposalId = await this.ethereumZDAO.lastProposalId();
-      return lastProposalId.toHexString();
+      return proposalId.toHexString();
     } catch (error: any) {
       const errorMsg = error?.data?.message ?? error.message;
       throw new FailedTxError(errorMsg);
